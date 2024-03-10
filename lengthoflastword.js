c5affb0ddab00b8fcae80915c02d54c1968518e2
MSK-SHIJAS <159407102+MSK-SHIJAS@users.noreply.github.com>
2024-03-10 20:50:35 +0530

    Create lengthoflastword.js

---
+++ lengthoflastword.js
@@ -0,0 +1,14 @@
+var lengthOfLastWord = function(s) {
+            let len = 0;
+        
+            x = s.trim();
+        
+            for (let i = 0; i < x.length; i++) {
+                if (x[i] == ' ') {
+                    len = 0;
+                }
+                else {
+                    len++;
+                }
+            }
+        
